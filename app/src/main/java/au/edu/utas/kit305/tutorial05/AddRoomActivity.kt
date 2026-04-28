a429c4b4dc907325ab51e2e45323aad28d2c624b
MattStarHD <matthewdunn03@gmail.com>
2026-04-28 23:57:12 +1000

    More improvements to add room activity (still issues)

--- app/src/main/java/au/edu/utas/kit305/tutorial05/AddRoomActivity.kt
+++ app/src/main/java/au/edu/utas/kit305/tutorial05/AddRoomActivity.kt
@@ -10,28 +10,34 @@ import android.widget.EditText
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AddRoomActivity : AppCompatActivity() {
-    val houseId = intent.getStringExtra("houseId") ?: ""
 
-    val txtRoomName = findViewById<EditText>(R.id.txtRoomName)
-    val txtRoomWidth = findViewById<EditText>(R.id.txtRoomWidth)
-    val txtRoomDepth = findViewById<EditText>(R.id.txtRoomDepth)
-    val btnSaveRoom = findViewById<Button>(R.id.btnSaveRoom)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_room)
 
-    val db = FirebaseFirestore.getInstance()
+        val houseId = intent.getStringExtra("houseId") ?: ""
 
-    btnSaveRoom.setOnClickListener {
-        val room = hashMapOf(
-            "houseId" to houseId,
-            "roomName" to txtRoomName.text.toString(),
-            "width" to txtRoomWidth.text.toString().toDoubleOrNull(),
-            "depth" to txtRoomDepth.text.toString().toDoubleOrNull(),
-            "notes" to ""
-        )
+        val txtRoomName = findViewById<EditText>(R.id.txtRoomName)
+        val txtRoomWidth = findViewById<EditText>(R.id.txtRoomWidth)
+        val txtRoomDepth = findViewById<EditText>(R.id.txtRoomDepth)
+        val btnSaveRoom = findViewById<Button>(R.id.btnSaveRoom)
 
-        db.collection("rooms")
-            .add(room)
-            .addOnSuccessListener {
-                finish()
-            }
+        val db = FirebaseFirestore.getInstance()
+
+        btnSaveRoom.setOnClickListener {
+            val room = hashMapOf(
+                "houseId" to houseId,
+                "roomName" to txtRoomName.text.toString(),
+                "width" to txtRoomWidth.text.toString().toDoubleOrNull(),
+                "depth" to txtRoomDepth.text.toString().toDoubleOrNull(),
+                "notes" to ""
+            )
+
+            db.collection("rooms")
+                .add(room)
+                .addOnSuccessListener {
+                    finish()
+                }
+        }
     }
 }
